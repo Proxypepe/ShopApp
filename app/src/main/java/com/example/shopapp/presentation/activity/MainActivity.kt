6b22b69d7d6d0821bcb912697c68161951a9ee88
Proxypepe <sasha.golovin.51@bk.ru>
2022-03-30 14:32:05 +0300

    Added bottom navigation bar

--- app/src/main/java/com/example/shopapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/presentation/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.shopapp.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import com.example.shopapp.presentation.navigation.AppNavigation
 import com.example.shopapp.ui.theme.ShopAppTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 @ExperimentalPagerApi
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/shopapp/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/shopapp/presentation/navigation/AppNavigation.kt
@@ -1,47 +1,139 @@
 package com.example.shopapp.presentation.navigation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.shopapp.presentation.screen.DetailedScreen
+import com.example.shopapp.presentation.screen.CartScreen
 import com.example.shopapp.presentation.screen.MainPage
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 
 @Composable
 @ExperimentalPagerApi
+@ExperimentalMaterialApi
 fun AppNavigation() {
-    val bottomItems by remember {
-        mutableStateOf(listOf ("Home", "Search", "Cart", "History", "Profile"))
-       }
-    Surface{
-        val navController = rememberNavController()
-        Scaffold(
-            bottomBar = {
-                BottomNavigation {
-                    bottomItems.forEach { screen ->
-                        BottomNavigationItem(selected = false,
-                            onClick = { navController.navigate(screen) },
-                            label = { Text(screen) }, icon = { })
-                    }// Foreach
-                } //BottomNavigation
-            } // bottomBar
-        ) { innerPadding ->
-            NavHost(navController = navController, startDestination = "Home") {
-                composable("Home") {
-                    Box(modifier = androidx.compose.ui.Modifier.padding(innerPadding))
-                    {
-                        DetailedScreen()
-                    }
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                items = listOf(
+                    BottomNavItem(
+                        name = "Home",
+                        route = "home",
+                        icon = Icons.Default.Home
+                    ),
+                    BottomNavItem(
+                        name = "Search",
+                        route = "search",
+                        icon = Icons.Default.Search
+                    ),
+                    BottomNavItem(
+                        name = "Cart",
+                        route = "cart",
+                        icon = Icons.Default.ShoppingCart
+                    ),
+                    BottomNavItem(
+                        name = "Favorite",
+                        route = "favorite",
+                        icon = Icons.Default.Favorite
+                    ),
+                    BottomNavItem(
+                        name = "Profile",
+                        route = "profile",
+                        icon = Icons.Default.Person
+                    ),
+
+                    ),
+                navController = navController,
+                onItemClick = {
+                    navController.navigate(it.route)
+                }
+            )
+        } // bottomBar
+    ) { innerPadding ->
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") {
+                Box(modifier = Modifier.padding(innerPadding))
+                {
+                    MainPage()
                 }
             }
+            composable("search") {
+
+            }
+            composable("cart") {
+                CartScreen()
+            }
+            composable("favorite") {
+
+            }
+            composable("profile") {
+
+            }
+        }
+    }
+}
+
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.White,
+                onClick = { onItemClick(item) },
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        if (item.badgeCount > 0) {
+                            BadgeBox(
+                                badgeContent = {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            ) {
+                                Icon(imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                        } else {
+                            Icon(imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                        }
+                        Text(text = item.name,
+                            textAlign = TextAlign.Center,
+                            fontSize = 10.sp
+                        )
+                    }
+                }
+            )
         }
     }
 }
