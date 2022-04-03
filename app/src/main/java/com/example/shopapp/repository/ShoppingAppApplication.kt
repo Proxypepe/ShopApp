d3ac4c89c5bf30cb9dec74fe329528a3ecc2442c
Proxypepe <sasha.golovin.51@bk.ru>
2022-04-03 17:51:36 +0300

    added reps

--- app/src/main/java/com/example/shopapp/repository/ShoppingAppApplication.kt
+++ app/src/main/java/com/example/shopapp/repository/ShoppingAppApplication.kt
@@ -1,7 +1,31 @@
 package com.example.shopapp.repository
 
 import android.app.Application
+import com.example.shopapp.repository.remote.RetrofitFactory
+import com.example.shopapp.repository.remote.api.*
+import com.example.shopapp.repository.remote.repository.*
 
 class ShoppingAppApplication : Application(){
+
+    private val authApi by lazy { RetrofitFactory().getInstance<AuthApi>("http://10.0.2.2:8000/")}
+    val authRepository by lazy { AuthRepository(authApi) }
+
+    private val registerApi by lazy { RetrofitFactory().getInstance<RegisterApi>("http://10.0.2.2:8000/register/")}
+    val registerRepository by lazy { RegisterRepository(registerApi) }
+
+    private val cartApi by lazy { RetrofitFactory().getInstance<CartApi>("http://10.0.2.2:8000/bag/")}
+    val cartRepository by lazy { CartRepository(cartApi) }
+
+    private val commentApi by lazy { RetrofitFactory().getInstance<CommentApi>("http://10.0.2.2:8000/comments/")}
+    val commentRepository by lazy { CommentRepository(commentApi) }
+
+    private val favoriteApi by lazy { RetrofitFactory().getInstance<FavoritesApi>("http://10.0.2.2:8000/favorite/")}
+    val favoriteRepository by lazy { FavoriteRepository(favoriteApi) }
+
+    private val productApi by lazy { RetrofitFactory().getInstance<ProductApi>("http://10.0.2.2:8000/products/")}
+    val productRepository by lazy { ProductRepository(productApi) }
+
+    private val ratingApi by lazy { RetrofitFactory().getInstance<RatingApi>("http://10.0.2.2:8000/rating/")}
+    val ratingRepository by lazy { RatingRepository(ratingApi) }
 
 }
